94d6635ff56284d8c235da472c352602b4455e42
hishamco <hishamco_2007@hotmail.com>
2015-01-23 15:02:54 +0300

    Add partials.js script

---
+++ partials.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    var elements = document.querySelectorAll('[data-include]');
+    for (var i = 0; i <= elements.length; i++) {
+        elements[i].innerHTML = httpGet(elements[i].getAttribute('data-include'));
+    }
+};
+
+function httpGet(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send();
+    if (xhr.readyState == 4 && xhr.status == 200)
+        return xhr.responseText;
+}
